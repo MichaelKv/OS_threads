e09b7adeb115298c38f2a4010c447a67f2998656
Michael Kozlov <michael.2212.k@gmail.com>
2015-10-04 22:00:35 +0300

    Add header for queue

--- sum_from_numbers_of_input.c
+++ sum_from_numbers_of_input.c
@@ -5,6 +5,15 @@
 #include <pthread.h>
 #include <unistd.h>
 
+// Queue header start----------------------------------------------------------->
+struct queue;
+struct queue *queue_create(int maxsize);
+int queue_enqueue(struct queue *q, char *value);
+char* queue_dequeue(struct queue *q);
+int queue_size(struct queue *q);
+void queue_free(struct queue *q);
+// Queue header end------------------------------------------------------------->
+
 int main() {
-	
+
 }
